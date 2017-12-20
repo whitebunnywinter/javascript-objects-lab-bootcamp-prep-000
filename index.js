ba8ed1436fe027b46d7b05b5215ccd28cf5bc108
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-20 09:21:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({} , object, {[key]: value})
   return newObj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  Object.assign(object, {[key]: value})
+  return object
+}
